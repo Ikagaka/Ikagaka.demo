50a432ecd9d81875a3e9708b98dbdb7ba08a9ffa
Narazaka <info@narazaka.net>
2015-05-23 01:00:10 +0900

    satori ignore saori

--- vendor/js/satoriworker.js
+++ vendor/js/satoriworker.js
@@ -23,6 +23,39 @@ var worker_code = "\"(C) 2014 Narazaka : Licensed under The BSD 3-Clause License
       return SatoriWorkerClient.__super__.load.call(this, dirpath);
     };
 
+    SatoriWorkerClient.prototype._push = function(dirpath) {
+      return this.storage.ghost_master(this.ghostpath).then(function(directory) {
+        return directory.asArrayBuffer();
+      }).then((function(_this) {
+        return function(directory) {
+          var data, path, ref, transferable;
+          transferable = [];
+          ref = _this._satori_ignore_saori(directory);
+          for (path in ref) {
+            data = ref[path];
+            transferable.push(data);
+          }
+          return _this.worker().request('push', [dirpath, directory], transferable);
+        };
+      })(this));
+    };
+
+    SatoriWorkerClient.prototype._satori_ignore_saori = function(directory) {
+      var filestr, path, uint8arr;
+      for (path in directory) {
+        if (/\bsatori_conf\.txt$/.test(path)) {
+          uint8arr = new Uint8Array(directory[path]);
+          filestr = Encoding.codeToString(Encoding.convert(uint8arr, 'UNICODE', 'SJIS'));
+          if (/＠SAORI/.test(filestr)) {
+            filestr = filestr.replace(/＠SAORI/, '＠NO__SAORI');
+            directory[path] = new Uint8Array(Encoding.convert(Encoding.stringToCode(filestr), 'SJIS', 'UNICODE')).buffer;
+          }
+          break;
+        }
+      }
+      return directory;
+    };
+
     return SatoriWorkerClient;
 
   })(NativeShioriWorkerClient);
